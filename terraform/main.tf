6db99a61550c416799361164b2a7cdac10a800d7
fda2026 <jamil.ceo.abiodun@gmail.com>
2026-03-09 14:56:00 +0100

    remove dynamodb

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-eks-state-123456"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
+    #dynamodb_table = "terraform-lock"
   }
 }
 
