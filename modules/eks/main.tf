86e3b222f70f4aab54f8b89c4a4138e5196c6c23
fda2026 <jamil.ceo.abiodun@gmail.com>
2026-03-09 15:48:15 +0100

    still working on the subnet added endpoints

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -2,6 +2,8 @@ resource "aws_eks_cluster" "eks" {
   name     = var.cluster_name
   role_arn = var.cluster_role
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids              = var.subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access  = true
   }
 }
