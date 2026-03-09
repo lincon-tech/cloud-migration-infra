51f0031792e0137c91d36cfacecfaafcfade6855
fda2026 <jamil.ceo.abiodun@gmail.com>
2026-03-09 14:16:05 +0100

    Adding worker nodes config

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,6 +1,17 @@
 resource "aws_eks_cluster" "eks" {
   name     = var.cluster_name
+  node_group_name = "migration-workers"
   role_arn = var.cluster_role
+
+  instance_types = var.node_instance_type
+
+  scaling_config {
+
+    desired_size = var.desired_size
+    max_size     = var.max_size
+    min_size     = var.min_size
+
+  }
 
   vpc_config {
     subnet_ids = var.subnet_ids
--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -4,6 +4,36 @@ variable "subnet_ids" {}
 
 variable "vpc_id" {}
 
+variable "node_instance_type" {
+  description = "Instance type for worker nodes"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 3
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 4
+}
+
+variable "min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 2
+}
+
 variable "cluster_role" {
   default = "arn:aws:iam::953831643964:role/eksClusterRole"
 }
