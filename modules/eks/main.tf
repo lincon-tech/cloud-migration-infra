9a6f61048106d3cd1f34bf248ab9567dec25eaa4
fda2026 <jamil.ceo.abiodun@gmail.com>
2026-03-09 16:46:42 +0100

    Added another config in eks main.tf and var.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -7,10 +7,41 @@ resource "aws_eks_cluster" "eks" {
   }
 }
 
+resource "aws_iam_role" "node_role" {
+  name = "${var.cluster_name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "worker_node" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "registry_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
 resource "aws_eks_node_group" "worker_nodes" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "${var.cluster_name}-workers"
-  #node_role_arn   = aws_iam_role.node_role.arn
+  node_role_arn   = aws_iam_role.node_role.arn
   subnet_ids      = var.subnet_ids
 
   scaling_config {
@@ -21,4 +52,9 @@ resource "aws_eks_node_group" "worker_nodes" {
 
   instance_types = ["t3.medium"]
 
+  depends_on = [
+    aws_iam_role_policy_attachment.worker_node,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.registry_policy
+  ]
 }
